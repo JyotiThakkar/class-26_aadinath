d638841c039c5ee01c34ed8bcdf1911b80105d35
JyotiThakkar <thakkarjyoti1@yahoo.in>
2021-10-17 15:41:47 +0530

    class 26 First Commit

--- sketch.js
+++ sketch.js
@@ -12,11 +12,12 @@ function preload() {
 
 function setup(){
     var canvas = createCanvas(1200,400);
+
     engine = Engine.create();
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(620,height,1200,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -34,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+console.log("Hello");
 }
 
 function draw(){
